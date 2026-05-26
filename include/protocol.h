d431bd92d89ee2e17303f2901323619b775a88b4
SwastikPant <swastik_p@ee.iitr.ac.in>
2026-05-26 11:20:51 +0530

    Project setup: CMake build system and binary wire protocol definitions

---
+++ include/protocol.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+
+struct ChunkHeader
+{
+    uint64_t index;
+    uint32_t size;
+};
+struct FileMetaWire
+{
+    uint64_t total_size;
+    uint32_t chunk_size;
+    uint64_t chunk_count;
+};
